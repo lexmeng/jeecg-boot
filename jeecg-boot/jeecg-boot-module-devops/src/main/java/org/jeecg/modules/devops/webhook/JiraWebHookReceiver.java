1a19f304cd4abb880172c84c07ed9ff5bd6be0cc
zuoc <cheng.zuo@kyligence>
2022-08-16 12:33:20 +0800

    init

---
+++ jeecg-boot/jeecg-boot-module-devops/src/main/java/org/jeecg/modules/devops/webhook/JiraWebHookReceiver.java
@@ -0,0 +1,11 @@
+package org.jeecg.modules.devops.webhook;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/webhook/jira")
+@Slf4j
+public class JiraWebHookReceiver {
+}
