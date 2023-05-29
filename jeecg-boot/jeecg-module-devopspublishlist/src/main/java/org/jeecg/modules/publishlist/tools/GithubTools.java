c3d8f09b6499db9b0857ea5b4c038299b15ed3c8
Cheng <Cheng.zuo@kyligence.io>
2023-05-29 14:48:16 +0800

    wip (#64)

--- jeecg-boot/jeecg-module-devopspublishlist/src/main/java/org/jeecg/modules/publishlist/tools/GithubTools.java
+++ jeecg-boot/jeecg-module-devopspublishlist/src/main/java/org/jeecg/modules/publishlist/tools/GithubTools.java
@@ -1,14 +1,20 @@
 package org.jeecg.modules.publishlist.tools;
 
+import com.google.common.collect.Lists;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.GHCommitStatus;
+import org.kohsuke.github.GHPullRequest;
+import org.kohsuke.github.GHPullRequestCommitDetail;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -23,34 +29,40 @@ public class GithubTools {
     @Value("${github.token}")
     private String GITHUB_TOKEN;
 
-    private String repo = "";
 
-    private void getPRAction(){
-        String url = GITHUB_BASE_URL + "/repos/" + GITHUB_ACCOUNT + "/" + repo + "/pulls";
+    private volatile GitHub gitHub;
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "token "+GITHUB_TOKEN);
-        List<MediaType> mediaTypes = new ArrayList<>();
-        mediaTypes.add(MediaType.APPLICATION_JSON);
-        headers.setAccept(mediaTypes);
+    @SneakyThrows
+    public GHRepository getRepository(String name) {
+        return connect().getRepository(name);
+    }
 
-        HttpEntity<Void> entity = new HttpEntity<>(headers);
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<List<PullRequest>> response = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<List<PullRequest>>() {
+    @SneakyThrows
+    public GitHub connect() {
+        if (gitHub == null) {
+            synchronized (this) {
+                if (gitHub == null) {
+                    gitHub = GitHub.connect(GITHUB_ACCOUNT, GITHUB_TOKEN);
+                }
+            }
+        }
+
+        return gitHub;
+    }
+
+    @SneakyThrows
+    public GHPullRequest pullRequest(GHRepository repository, int id) {
+        return repository.getPullRequest(id);
+    }
+
+    @SneakyThrows
+    public List<GHCommitStatus> status(GHPullRequest pullRequest) {
+        final GHPullRequestCommitDetail lastCommit = pullRequest.listCommits().toList().get(pullRequest.getCommits() - 1);
+        final Map<String, GHCommitStatus> status = new HashMap<>();
+        pullRequest.getRepository().getCommit(lastCommit.getSha()).listStatuses().forEach((s) -> {
+            status.putIfAbsent(s.getContext(), s);
         });
-        List<PullRequest> prList = response.getBody();
-        for(PullRequest pr : prList){
-            System.out.println(pr.getTitle());
-        }
+        return Lists.newArrayList(status.values());
     }
 
-    private static class PullRequest{
-        private String title;
-        public String getTitle(){
-            return title;
-        }
-        public void setTitle(String title){
-            this.title = title;
-        }
-    }
 }
