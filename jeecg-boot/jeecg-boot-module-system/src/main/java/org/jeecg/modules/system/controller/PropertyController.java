d2e6cfa7e5f86946b5ed6fb17c9d79a4cca2663e
qlf128 <93907730@qq.com>
2023-06-21 13:45:35 +0800

    fix bug (#93)

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/controller/PropertyController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/system/controller/PropertyController.java
@@ -4,7 +4,6 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.jeecg.common.api.vo.Result;
-import org.jeecg.modules.publishlist.tools.SpringContextProvider;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
